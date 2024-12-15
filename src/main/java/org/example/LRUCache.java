78f2a5e9f18344622de42895bff7a4a854e248b3
AlexPapagre <apapagrigoriou17@gmail.com>
2024-12-16 01:57:12 +0200

    Added Node class and size in LRUCache class

--- src/main/java/org/example/LRUCache.java
+++ src/main/java/org/example/LRUCache.java
@@ -1,6 +1,15 @@
 package org.example;
 
 public class LRUCache<K, V> implements Cache<K, V> {
+    private Node<K, V> head, tail;
+    private int maxSize, size;
+
+    public LRUCache(int maxSize) {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+        this.maxSize = maxSize;
+    }
 
     @Override
     public V get(K key) {
@@ -10,6 +19,12 @@ public class LRUCache<K, V> implements Cache<K, V> {
     @Override
     public void put(K key, V value) {
 
+    }
+
+    private static class Node<K, V> {
+        public K key;
+        public V value;
+        public Node<K, V> next, prev;
     }
 
 }
