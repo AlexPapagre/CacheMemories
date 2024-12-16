161b701942b5cc73e2e03ee965bfe2433378387d
AlexPapagre <apapagrigoriou17@gmail.com>
2024-12-16 02:29:37 +0200

    Added junit tests and updated pom.xml for junit

---
+++ src/test/java/org/example/LRUCacheTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class LRUCacheTest {
+
+    private static final int SIZE = 1;
+
+    @Test
+    public void testLRUCache() {
+
+    }
+}
