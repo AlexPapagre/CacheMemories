91a0e9a169cf818de09d76977ae52730246c654b
AlexPapagre <apapagrigoriou17@gmail.com>
2024-12-18 01:40:45 +0200

    Fixed Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,38 +10,50 @@ public class Main {
 
         Cache<Integer, Integer> cache = new LRUCache<>();
 
-        Random rng = new Random();
-        
-        for (int i = 0; i < Math.sqrt(OPERATIONS); i++) {
+        int count = 0;
+        while (count < OPERATIONS) {
 
-            // Put integers in cache
-            for (int j = 0; j < Math.sqrt(OPERATIONS); j++) {
-
-                // 80% chance num belongs to [0 - 49999]
-                // 20% chance num belongs to [50000 - 99999]
-                int min = 0, max = 49999;
-                if (rng.nextInt(5) == 0) {
-                    min = 50000;
-                    max = 99999;
-                }
-                int num = rng.nextInt(max - min + 1) + min;
+            // Put 1000 integers to cache
+            for (int i = 0; i < 1000; i++) {
+                int num = getRandomInt();
                 cache.put(num, num);
             }
 
-            // Get integers in from cache
-            for (int j = 0; j < Math.sqrt(OPERATIONS); j++) {
-
-                // 80% chance num belongs to [0 - 49999]
-                // 20% chance num belongs to [50000 - 99999]
-                int min = 0, max = 49999;
-                if (rng.nextInt(5) == 0) {
-                    min = 50000;
-                    max = 99999;
+            // Get 1000 integers from cache
+            for (int i = 0; i < 1000; i++) {
+                cache.get(getRandomInt());
+                count++;
+                if (count >= OPERATIONS) {
+                    break;
                 }
-                int num = rng.nextInt(max - min + 1) + min;
-                cache.get(num);
             }
 
+            count++;
         }
+        count--;
+
+        // Print results
+        System.out.println("Total operations: " + count);
+        System.out.println("Cache Hits: " + cache.getHitCount());
+        System.out.println("Cache Misses: " + cache.getMissCount());
+        System.out.println("Hit Rate: " + String.format("%.2f", (double) cache.getHitCount() / (cache.getHitCount() + cache.getMissCount()) * 100) + "%");
+        System.out.println("Miss Rate: " + String.format("%.2f", (double) cache.getMissCount() / (cache.getHitCount() + cache.getMissCount()) * 100) + "%");
+
+
     }
+
+    private static int getRandomInt() {
+        Random rng = new Random();
+
+        // 80% chance num belongs to [0 - 49999]
+        // 20% chance num belongs to [50000 - 99999]
+        int min = 0, max = 49999;
+        if (rng.nextInt(5) == 0) {
+            min = 50000;
+            max = 99999;
+        }
+
+        return rng.nextInt(max - min + 1) + min;
+    }
+
 }
