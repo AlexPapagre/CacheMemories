b6209ee6f03905256a4db8c36baa9224823011e9
AlexPapagre <apapagrigoriou17@gmail.com>
2024-12-17 21:05:00 +0200

    Updated main rand with sqrt

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,24 +3,32 @@ package org.example;
 import java.util.Random;
 
 public class Main {
+
+    public static final int OPERATIONS = 100000;
+
     public static void main(String[] args) {
 
         Cache<Integer, Integer> cache = new LRUCache<>();
 
         Random rng = new Random();
+        
+        for (int i = 0; i < Math.sqrt(OPERATIONS); i++) {
 
-        // Put 5000 integers in cache
-        for (int i = 0; i < 5000; i++) {
+            // Put integers in cache
+            for (int j = 0; j < Math.sqrt(OPERATIONS); j++) {
 
-            // 80% chance num belongs to [0 - 49999]
-            // 20% chance num belongs to [50000 - 99999]
-            int min = 0, max = 49999;
-            if (rng.nextInt(5) == 0) {
-                min = 50000;
-                max = 99999;
+                // 80% chance num belongs to [0 - 49999]
+                // 20% chance num belongs to [50000 - 99999]
+                int min = 0, max = 49999;
+                if (rng.nextInt(5) == 0) {
+                    min = 50000;
+                    max = 99999;
+                }
+                int num = rng.nextInt(max - min + 1) + min;
+                cache.put(num, num);
             }
-            int num = rng.nextInt(max - min + 1) + min;
-            cache.put(num, num);
+
+            // Get
         }
     }
 }
