a8e7926c17cf6a9b21cf8d9b77d9e6e80ed82e36
AlexPapagre <apapagrigoriou17@gmail.com>
2024-12-18 01:56:44 +0200

    Added enum

---
+++ src/main/java/org/example/CacheReplacementPolicy.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public enum CacheReplacementPolicy {
+    LRU("Least Recently Used"),
+    MRU("Most Recently Used");
+
+    private final String description;
+
+    CacheReplacementPolicy(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+
+}
