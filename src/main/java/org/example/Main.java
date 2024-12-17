bbafa5217fb3f4004273d47d044c662f2719d66e
AlexPapagre <apapagrigoriou17@gmail.com>
2024-12-17 21:13:26 +0200

    Finished 80 20 random numbers

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -28,7 +28,20 @@ public class Main {
                 cache.put(num, num);
             }
 
-            // Get
+            // Get integers in from cache
+            for (int j = 0; j < Math.sqrt(OPERATIONS); j++) {
+
+                // 80% chance num belongs to [0 - 49999]
+                // 20% chance num belongs to [50000 - 99999]
+                int min = 0, max = 49999;
+                if (rng.nextInt(5) == 0) {
+                    min = 50000;
+                    max = 99999;
+                }
+                int num = rng.nextInt(max - min + 1) + min;
+                cache.get(num);
+            }
+
         }
     }
 }
