ddeb4bdb067b8d2dfacdffdbf8501231d671b562
AlexPapagre <apapagrigoriou17@gmail.com>
2024-12-18 19:26:55 +0200

    Added comments to tests

--- src/test/java/org/example/CacheTest.java
+++ src/test/java/org/example/CacheTest.java
@@ -12,6 +12,7 @@ public class CacheTest {
     public void testLRUCache() {
         Cache<Integer, Integer> cache = new CacheImpl<>(CAPACITY, CacheReplacementPolicy.LRU);
 
+        // Puts 'CAPACITY * 2' integers in cache and checks if they are saved properly
         for (int i = 0; i < CAPACITY * 2; i++) {
             assertNull(cache.get(i));
             cache.put(i, i);
@@ -23,12 +24,14 @@ public class CacheTest {
     public void testLRUCacheEdgeCases() {
         Cache<Integer, Integer> cache = new CacheImpl<>(CAPACITY, CacheReplacementPolicy.LRU);
 
+        // Puts 'CAPACITY' integers in cache and checks if they are saved properly
         for (int i = 0; i < CAPACITY; i++) {
             assertNull(cache.get(i));
             cache.put(i, i);
             assertEquals((int) cache.get(i), i);
         }
 
+        // Puts 'CAPACITY' integers in cache and checks if the least recent integer got removed
         for (int i = CAPACITY; i < CAPACITY * 2; i++) {
             cache.put(i, i);
             assertNull(cache.get(i - CAPACITY));
@@ -39,12 +42,14 @@ public class CacheTest {
     public void stressTestLRUCache() {
         Cache<Integer, Integer> cache = new CacheImpl<>(CAPACITY, CacheReplacementPolicy.LRU);
 
+        // Puts 1000000 integers in cache and checks if they are saved properly
         for (int i = 0; i < 1000000; i++) {
             assertNull(cache.get(i));
             cache.put(i, i);
             assertEquals((int) cache.get(i), i);
         }
 
+        // Checks if the last 'CAPACITY' integers are saved and checks if the other integers got removed
         for (int i = 0; i < 1000000; i++) {
             if (i >= 1000000 - CAPACITY) {
                 assertEquals((int) cache.get(i), i);
@@ -58,6 +63,7 @@ public class CacheTest {
     public void testMRUCache() {
         Cache<Integer, Integer> cache = new CacheImpl<>(CAPACITY, CacheReplacementPolicy.MRU);
 
+        // Puts 'CAPACITY * 2' integers in cache and checks if they are saved properly
         for (int i = 0; i < CAPACITY * 2; i++) {
             assertNull(cache.get(i));
             cache.put(i, i);
@@ -69,12 +75,14 @@ public class CacheTest {
     public void testMRUCacheEdgeCases() {
         Cache<Integer, Integer> cache = new CacheImpl<>(CAPACITY, CacheReplacementPolicy.MRU);
 
+        // Puts 'CAPACITY' integers in cache and checks if they are saved properly
         for (int i = 0; i < CAPACITY; i++) {
             assertNull(cache.get(i));
             cache.put(i, i);
             assertEquals((int) cache.get(i), i);
         }
 
+        // Puts 'CAPACITY' integers in cache and checks if the most recent integer got removed
         for (int i = CAPACITY; i < CAPACITY * 2; i++) {
             assertEquals((int) cache.get(i - CAPACITY), i - CAPACITY);
             cache.put(i, i);
@@ -86,12 +94,14 @@ public class CacheTest {
     public void stressTestMRUCache() {
         Cache<Integer, Integer> cache = new CacheImpl<>(CAPACITY, CacheReplacementPolicy.MRU);
 
+        // Puts 1000000 integers in cache and checks if they are saved properly
         for (int i = 0; i < 1000000; i++) {
             assertNull(cache.get(i));
             cache.put(i, i);
             assertEquals((int) cache.get(i), i);
         }
 
+        // Checks if the first 'CAPACITY - 1' integers and the last integer (99999) are saved and checks if the other integers got removed
         for (int i = 0; i < 1000000; i++) {
             if (i < CAPACITY - 1 || i == 999999) {
                 assertEquals((int) cache.get(i), i);
